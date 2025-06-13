39c7b267aca40cdd82a6942264cf55eb42e84b6c
Manuel Ardila <ardilasanchezmanuelsteven@gmail.com>
2025-06-13 12:07:51 -0500

    Con controladores

--- src/main.js
+++ src/main.js
@@ -1,1 +1,58 @@
 import "./styles.css"
+
+import * as ControladorCategories from "./Views/Categories/ControladorCategories.js"
+import * as ProductsController from  "./Views/Categories/ProductsController.js"
+
+let rutas = [
+         {
+          name: "Categories",
+          path: "./src/views/Categories/index.html",
+          controlador: ControladorCategories.init
+         } ,
+         {
+            name:"Products",
+            path:"./src/views/Products/index.html",
+            controlador: ProductsController.init
+
+         }
+
+]
+
+    
+
+
+
+
+   
+
+window.addEventListener("hashchange",(Event)=>{
+     
+    const ruta = location.hash.slice(1)
+
+
+     rutas.forEach(objeto =>{
+          
+        if(objeto.name == ruta){
+
+            fetch(objeto.path).then((response)=>{
+                response.text().then(html=>{
+                    document.querySelector(".contenido").innerHTML = html
+                    objeto.controlador()
+
+                })
+            })
+
+        }
+
+
+     })
+   
+
+   
+
+})
+
+
+
+
+
